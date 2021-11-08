2e23b9ac9ea233748002cf40c2d91de0df599a32
trairatp-sk <79890842+trairatp-sk@users.noreply.github.com>
2021-11-08 20:37:34 +0700

    Initialize demo-02-fixing-mistake

---
+++ src/functions/index.ts
@@ -0,0 +1,23 @@
+export const add = (a: number, b: number): number => {
+  return a + b
+}
+
+export const subtract = (a: number, b: number): number => {
+  return a - b
+}
+
+export const multiply = (a: number, b: number): number => {
+  return a * b
+}
+
+export const divide = (a: number, b: number): number => {
+  return a / b
+}
+
+export const pow = (a: number, b: number): number => {
+  return a ** b
+}
+
+export const square = (a: number): number => {
+  return a ** 2
+}
